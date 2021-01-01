3bd47c0eb33746ff0b1a8b37886b1c159c81faa5
hllee1021 <35593748+hllee1021@users.noreply.github.com>
2021-01-02 03:43:30 +0900

    [feat] new build and make config file to save mongodb connection

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser=require('body-parser')
+const config=require('./config/key')
+const {User}=require("./models/User")
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const mongoose=require('mongoose')
-mongoose.connect('mongodb://localhost:27017',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('Mongodb success~'))
 .catch(err=>console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! gogogo')
+})
+
+app.post('/register',(req,res)=>{
+    
+    const user=new User(req.body)
+
+    user.save((err,userInfo) => {
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
 })
 
 app.listen(port, () => {
